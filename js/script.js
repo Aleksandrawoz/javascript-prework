808b5d3aa8e63fb48f49a4708b1bab47d6c08da0
ola wozniak <olawozniak551@gmail.com>
2021-06-08 09:28:03 +0200

    z warunkiem if...else

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
@@ -19,3 +23,30 @@ if (randomNumber == 3) {
     computerMove = 'nożyce ';
 }
 printMessage('Mój ruch to: ' + computerMove);
+
+
+
+
+if (computerMove == 'kamień' && playerMove == 'papier') {
+    printMessage('Ty wygrywasz!');
+}
+
+else if (computerMove == 'papier' && playerMove == 'nożyce') {
+    printMessage('Ty wygrywasz!');
+}
+
+else if (computerMove == 'kamień' && playerMove == 'kamień') {
+    printMessage('Mamy remis!');
+}
+
+else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+}
+
+else {
+    printMessage("Ty przegrywasz")
+}
+
+}
+
+if (computerMove == 'nieznany ruch')
